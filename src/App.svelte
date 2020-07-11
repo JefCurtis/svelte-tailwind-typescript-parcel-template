53698b03df7af8271ee1b7704f795de4e89c9dfd
jefcurtis <jef.curtis@clio.com>
2020-07-11 00:35:56 -0700

    added hello world text

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,8 @@
 <script lang="typescript">
   import Loader from "./components/loader.svelte";
+  const text = "Hello world!";
 </script>
 
 <div class="m-4 flex justify-center">
-  <Loader />
+  <Loader {text} />
 </div>
--- src/components/loader.svelte
+++ src/components/loader.svelte
@@ -1,5 +1,9 @@
+<script lang="typescript">
+  export let text: string;
+</script>
+
 <div
   class="border border-gray-500 rounded shadow-md px-3 bg-white py-10 flex justify-center items-center max-w-md w-full">
   <span class="text-4xl pr-4">🙈</span>
-  <h1 class="text-xl font-bold text-gray-500">Loading...</h1>
+  <h1 class="text-xl font-bold text-gray-500">{text}</h1>
 </div>
